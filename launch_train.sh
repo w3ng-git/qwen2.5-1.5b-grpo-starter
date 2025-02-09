a8b0e3ca637f42951bd5d00c3dc9dd59c7a90832
w3ng-git <asset1016@gmail.com>
2025-02-09 19:15:23 +0800

    Update launch_train.sh

--- launch_train.sh
+++ launch_train.sh
@@ -1,1 +1,4 @@
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file zero3.yaml --num_processes=7 train_grpo.py
+!ACCELERATE_LOG_LEVEL=info accelerate launch \
+--config_file zero3.yaml \
+--num_processes=3 \
+train_grpo.py \
